90b16100b7f0addd3b3987c2d406432d47d0b363
ahlembns <ahlem.benslama92@gmail.com>
2024-02-26 15:52:24 +0000

    test

--- backend_confirmed/solution.js
+++ backend_confirmed/solution.js
@@ -1,32 +1,34 @@
-const { argv } = require("node:process");
-const fs = require("fs");
-const { jsonrepairTransform } = require('jsonrepair/stream')
+const fs = require('fs');
+const readline = require('readline');
 
+// Get the id from the command line arguments
+const id = process.argv[2];
+console.log(id)
+// Create a readline interface to read the file line by line
+const rl = readline.createInterface({
+  input: fs.createReadStream('input.json'),
+  crlfDelay: Infinity
+});
 
-const readFile = async (userId, filePath) => {
-const inputStream = fs.createReadStream(filePath)
-
-inputStream
-  .pipe(jsonrepairTransform())
-  .on('data', (chunk)=> { 
-    const parsedChunk =  JSON.parse(chunk.toString())
-    if(parsedChunk.id == userId) {
-       inputStream.close();
+rl.on('line', (line) => {
+  try {
+    const obj = JSON.parse(line);
+    if (obj.id === id) {
+      console.log(obj.name);
+      rl.close();
     }
-  })
-  .on('error', (err) => console.error(err))
-  .on('finish', () => console.log('done'))
-
-
-};
-main = async () => {
-  const id = argv[2];
-  if (isNaN(id)) {
-    throw new Error(
-      `third argument should be a number instead we have got ${id}`
-    );
-  } else {
-    readFile(id,"input.json");
+  } catch (error) {
+    console.error('Error parsing JSON:', error.message);
   }
-};
-main();
+
+});
+
+// Log the object found with the given id is  
+rl.on('close', () => {
+  console.log('Object with id ' + id );
+});
+// Log an error if the object with the given id is not found
+rl.on('error', () => {
+  console.log('error while searching object with id'  + id );
+});
+
